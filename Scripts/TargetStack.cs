3f22fa3d81adefae74afe34d2fe2f9cd37bd24e5
5p1ke5 <bstrazdas@gmail.com>
2023-10-22 18:56:04 -0500

    Initial commit.

---
+++ Scripts/TargetStack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetStack : Target
+{
+    public float impactForce;
+
+    Rigidbody targetRigidbody;
+
+    void Start()
+    {
+        targetRigidbody = target.GetComponent<Rigidbody>();
+    }
+
+    public override void Process(RaycastHit hit)
+    {
+        targetRigidbody.AddForce(-hit.normal * impactForce);
+
+        effectScript.Play(hit, hitSound, hitEffect, effectDuration);
+    }
+}
