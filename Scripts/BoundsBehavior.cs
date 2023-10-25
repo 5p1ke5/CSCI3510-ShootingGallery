76093b73d001596c83986035729a99a6aa991942
Sami Stuart <samistuart@MacBook-Pro-158.local>
2023-10-24 22:18:18 -0500

    102323_2151Materials, added bounds, materials package made all the stuff glowing and pink

---
+++ Scripts/BoundsBehavior.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundsBehavior : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+{
+        if (!other.gameObject.CompareTag( "Player"))
+        {
+
+            Debug.Log("not a player");
+            Debug.Log(other.gameObject.tag);
+            Physics.IgnoreCollision(other.gameObject.GetComponent<Collider>(), GetComponent<Collider>());
+        }
+
+
+    }
+
+
+}
